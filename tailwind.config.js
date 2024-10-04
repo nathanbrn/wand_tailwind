0299b4b541a5d7d6fd878588aa6939cd5a38d5b7
nathanbrn <breno.matos@maino.com.br>
2024-10-04 11:02:53 -0300

    chore: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/javascript/**/*.vue',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
